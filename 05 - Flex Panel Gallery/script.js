603e1a5427114e4ed5727c105fc5adf7de83756f
La-Ruda <87708653+La-Ruda@users.noreply.github.com>
2024-04-05 12:20:20 +0200

    cc

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -4,18 +4,18 @@ const panelSwitch = (event) => {
     panels.forEach((panel) => {
         if (panel.contains(event.target)) {
             panel.classList.toggle("panel--hovered");
-            panel.querySelector(".p__one").classList.toggle("p__one--closed")
-            panel.querySelector(".p__third").classList.toggle("p__third--closed")
         } else {
             panel.classList.remove("panel--hovered");
-            panel.querySelector(".p__one").classList.add("p__one--closed")
-            panel.querySelector(".p__third").classList.add("p__third--closed")
         }
     })
-}
+};
 
 panels.forEach((panel) => {
     panel.addEventListener("click", panelSwitch);
+    panel.addEventListener("transitionend", toggleActive = (e) => {
+        if (e.propertyName.includes("flex")) {
+            panel.querySelector(".p__one").classList.toggle("p__one--closed");
+            panel.querySelector(".p__third").classList.toggle("p__third--closed");
+        }
+    })
 });
-
-
